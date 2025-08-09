cc9fcf3696d8ab7347a23fdf3200800349a4b6f2
ZakariaKefo <kefo.info@gmail.com>
2025-08-09 21:35:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA cache for GitHub Pages (works under subpaths) */
-const CACHE = "kefo-static-v3";
+const CACHE = "kefo-static-v4";
 const ASSET_PATHS = [
   ".", "index.html", "manifest.webmanifest",
   "icons/icon.svg", "icons/icon-192.png", "icons/icon-512.png"
