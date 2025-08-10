c30b6a1ba4289482e209efc3f0b77ea87e1b85ab
KeFo <kefo.info@gmail.com>
2025-08-10 17:55:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kefo-static-v11";
+const CACHE = "kefo-static-v12";
 const ASSET_PATHS = [
   ".", "index.html", "manifest.webmanifest",
   "icons/icon.svg", "icons/icon-192.png", "icons/icon-512.png"
